8c1164e4446bd6f57e69ddffc3676b36c6e7f7ae
Kevin Chavez <kevinchavz24@gmail.com>
2018-10-30 16:42:01 -0500

    registro

--- index.php
+++ index.php
@@ -4,10 +4,37 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>ProyectoTaller2</title>
-</head>
+    <title>Registro</title>
+    <link rel="stylesheet" href="partes/styles.css">
+    <?php include 'partes/cabecera.php'?>
+</head> 
 <body>
-    <p>Probando</p>
-    <p>afsfa</p>
+    <div class = "registro">
+        <form action="procesar_registro"method ="post">
+            <div>
+                <input style="width:508px;height:50px" type="text" name ="nombre"placeholder = "Nombres">
+            </div>
+            <div>
+                <input class="input2" type="text" name ="apellidos" placeholder = "Apellidos">
+            </div>
+            <div>
+                <nav class="fecha">Fecha de nacimiento: </nav>
+                <input class="input5" type="date" name ="fechan" >
+            </div>
+            <div>
+                <input class="input3" type="email" name ="correo"placeholder = "Correo">
+            </div>
+            <div>
+                <input class="input4" type="password" name ="contraseña" placeholder = "contraseña">
+            </div>
+            <div>
+                <label for="">Masculino</label>
+                <input class="input6" type="radio" name ="masculino"placeholder = "M">
+                <label for="">Femenino</label>
+                <input class="input7" type="radio" name ="femenino"placeholder = "F">
+            </div>
+            <button style="width:300px;height:80px;margin-top:30px" type="submit">Registrarme</button>
+        </form>
+    </div>
 </body>
 </html>
